f58b26c47612f0e0a29b066de55ab06c108b8d4f
juanifernandez <juanifernandezvilarino@gmail.com>
2021-09-28 21:19:22 -0300

    WinForm project

---
+++ TP3ITS.WinForm/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace TP3ITS.WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.todoBox = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.QuitBtn = new System.Windows.Forms.Button();
+            this.ListBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // todoBox
+            // 
+            this.todoBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.todoBox.Font = new System.Drawing.Font("Unispace", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.todoBox.Location = new System.Drawing.Point(234, 33);
+            this.todoBox.Name = "todoBox";
+            this.todoBox.Size = new System.Drawing.Size(188, 21);
+            this.todoBox.TabIndex = 0;
+            this.todoBox.TextChanged += new System.EventHandler(this.todoBox_TextChanged);
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.submitBtn.Font = new System.Drawing.Font("Unispace", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(428, 33);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(106, 21);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Agregar Tarea";
+            this.submitBtn.UseVisualStyleBackColor = false;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // QuitBtn
+            // 
+            this.QuitBtn.BackColor = System.Drawing.Color.Red;
+            this.QuitBtn.Location = new System.Drawing.Point(535, 105);
+            this.QuitBtn.Name = "QuitBtn";
+            this.QuitBtn.Size = new System.Drawing.Size(189, 38);
+            this.QuitBtn.TabIndex = 3;
+            this.QuitBtn.Text = "Salir";
+            this.QuitBtn.UseVisualStyleBackColor = false;
+            this.QuitBtn.Click += new System.EventHandler(this.QuitBtn_Click);
+            // 
+            // ListBtn
+            // 
+            this.ListBtn.Location = new System.Drawing.Point(51, 105);
+            this.ListBtn.Name = "ListBtn";
+            this.ListBtn.Size = new System.Drawing.Size(183, 38);
+            this.ListBtn.TabIndex = 4;
+            this.ListBtn.Text = "Mostrar Tareas";
+            this.ListBtn.UseVisualStyleBackColor = true;
+            this.ListBtn.Click += new System.EventHandler(this.ListBtn_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(241, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ingresa un texto mayor a 3 caracteres. ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.todoBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListBtn);
+            this.Controls.Add(this.QuitBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Name = "Form1";
+            this.Text = "TODO LIST";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox todoBox;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button QuitBtn;
+        private System.Windows.Forms.Button ListBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TP3ITS.WinForm/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP3ITS.WinForm
+{
+    public partial class Form1 : Form
+    {
+        List<string> todos = new List<string>();
+public Form1()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(Application.StartupPath + @"\img\fondo.jpg");
+            this.BackgroundImage = img;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            submitBtn.Enabled = false;
+        }
+        private void todoBox_TextChanged(object sender, EventArgs e)
+        {
+            string currentTodo = todoBox.Text;
+            if (currentTodo.Length > 3)
+            {
+                submitBtn.Enabled = true;
+                label1.Hide();
+            }
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            string currentTodo = todoBox.Text;
+            todos.Add(currentTodo);
+            todoBox.Clear();
+        }
+
+        private void ListBtn_Click_1(object sender, EventArgs e)
+        {
+            string allTodos = string.Join("\n", todos);
+            if (allTodos == "") 
+            {
+               MessageBox.Show("No hay tareas pendientes por hacer"); 
+            }  else 
+                { 
+                    MessageBox.Show(allTodos); 
+                }
+        }
+
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
